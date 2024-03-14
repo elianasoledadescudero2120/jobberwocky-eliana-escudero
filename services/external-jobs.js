70a57ba7f4f6af40eacdbb82c4392da263303352
Eliana <eliana.escudero@porthos.com.ar>
2024-03-14 11:25:17 -0300

    Cambio de lugar

--- services/external-jobs.js
+++ services/external-jobs.js
@@ -1,8 +1,7 @@
 import config from '../config.js';
 
-const { EXTERNAL_JOBS_SOURCE } = process.env;
-
 const getExternalJobs = async (filters = '') => {
+  const { EXTERNAL_JOBS_SOURCE } = process.env;
   let url = EXTERNAL_JOBS_SOURCE ? new URL(EXTERNAL_JOBS_SOURCE) : new URL(config.externalJobsSource);
   url.search = new URLSearchParams(filters)
 
